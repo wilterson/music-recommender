bb92435868e2892b707cc77b0eb79aa5ac3b48c6
Wilterson Garcia <wilterson.garcia@hypery.co>
2023-05-15 21:44:47 -0300

    chore: added get audio features

--- index.py
+++ index.py
@@ -26,19 +26,44 @@ def app():
         code = st.experimental_get_query_params().get("code", None)
 
         if code:
-            token_info = sp_oauth.get_access_token(code)
+            token_info = sp_oauth.get_cached_token()
             session_state.token = token_info['access_token']
             sp = spotipy.Spotify(auth=session_state.token)
 
             results = sp.current_user_saved_tracks(limit=5)
             tracks = results['items']
 
+            track_ids = []
+
             for track in tracks:
+                track_ids.append(track['track']['id'])
+
+            track_features = sp.audio_features(track_ids)
+
+            features_dict = {}
+            for feature in track_features:
+                features_dict[feature['id']] = feature
+
+            for track in tracks:
+                track_id = track['track']['id']
+                if track_id in features_dict:
+                    track['audio_features'] = features_dict[track_id]
+
                 st.write(f"Name: {track['track']['name']}")
                 st.write(f"Artist: {track['track']['artists'][0]['name']}")
                 st.write(f"Album: {track['track']['album']['name']}")
-                st.write(f"Added at: {track['added_at']}")
+                st.write(f"Popularity: {track['track']['popularity']}")
+
+                st.write(f"Acousticness: {track['audio_features']['acousticness']}")
+                st.write(f"Danceability: {track['audio_features']['danceability']}")
+                st.write(f"Energy: {track['audio_features']['energy']}")
+                st.write(f"Instrumentalness: {track['audio_features']['instrumentalness']}")
+                st.write(f"Liveness: {track['audio_features']['liveness']}")
+                st.write(f"Loudness: {track['audio_features']['loudness']}")
+                st.write(f"Speechiness: {track['audio_features']['speechiness']}")
+                st.write(f"Valence: {track['audio_features']['valence']}")
                 st.write('---')
+
 
 # Run Streamlit app
 if __name__ == '__main__':
